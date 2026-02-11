03add9b456fbdf6f1313b7a8142ea6a45011263c
Sina Tesfaye <sinatesfaye38@gmail.com>
2026-02-12 01:47:51 +0300

    Added about page

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,12 +1,67 @@
- import logo from "./logo.png";
+import logo from "./logo.png";
 import about from "./about.png";
 import icon from "./icon.png";
-import left from './left.png'
-import right from './right.png'
+import left from "./left.png";
+import right from "./right.png";
+
+import project1 from "./project_img_1.png";
+import project2 from "./project_img_2.png";
+import project3 from "./project_img_3.png";
+import project4 from "./project_img_4.png";
+import project5 from "./project_img_5.png";
+import project6 from "./project_img_6.png";
+ 
+ 
+
 export const assets = {
   logo,
   about,
   icon,
   left,
-  right
+  right,
+  project1,
+  project2,
+  project3,
+  project4,
+  project5,
+  project6
 };
+
+export const projectsData = [
+  {
+    title: "Skyline Haven",
+    price: "$2,50,000",
+    location: "California",
+    image: project1,
+  },
+  {
+    title: "Vista Verde",
+    price: "$2,50,000",
+    location: "San Francisco",
+    image: project2,
+  },
+  {
+    title: "Ocean Breeze",
+    price: "$3,00,000",
+    location: "Florida",
+    image: project3,
+  },
+  {
+    title: "Urban Nest",
+    price: "$2,20,000",
+    location: "New York",
+    image: project4,
+  },
+  {
+    title: "Luxury Heights",
+    price: "$4,00,000",
+    location: "Los Angeles",
+    image: project5,
+  },
+    {
+    title: "Luxury Heights",
+    price: "$4,00,000",
+    location: "Los Angeles",
+    image: project6,
+  },
+];
